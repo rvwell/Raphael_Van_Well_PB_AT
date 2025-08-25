d997282a6a67cd1984b255fe8252548cbbb2fbf6
Raphael Van Well <rapha.well@outlook.com>
2025-08-25 18:17:12 -0300

    Initial

---
+++ backend/monolito/src/main/java/com/infnet/pb/AT/AtApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.pb.AT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtApplication.class, args);
+	}
+
+}
---
+++ backend/monolito/src/test/java/com/infnet/pb/AT/AtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.pb.AT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
